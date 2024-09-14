4106aab11c9f4047d917dee4688f65464f868b72
AmeliaOcha <yainisiapaa@gmail.com>
2024-09-14 20:40:39 +0700

    last_up_tls_pemograman.cpp

--- tls.pemograman.cpp
+++ tls.pemograman.cpp
@@ -3,12 +3,17 @@ using namespace std;
 
 int main(){
 	int n;
+	cout << "--Selamat datang di program sederhana untuk memnentukan nilai modus terbesar--";
+	cout << "Masukkan berapa banyak angka atau data yang akan kamu tentukan nilai modusnya : ";
 	cin >> n;
+	cout << endl;
+	cout << "Masukkan angka atau data : ";
 	vector<int> arrn(n);
 	map<int, int> modus;
 	for(int a = 0; a < n; a++){
 		cin >> arrn[a];
 	}
+	cout << endl;
 	for(int a = 0; a < n; a++){
 		modus[arrn[a]]++;
 	}
